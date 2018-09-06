09984caae6617f9a2f540d72e03c68898b3fd48a
FaceBook <tao_shuminX@foxmail.com>
2018-09-06 13:53:17 +0800

    admin 中文显示

--- ProjectCrm/ProjectCrm/settings.py
+++ ProjectCrm/ProjectCrm/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',  # SessionMiddleware 之后设置中文显示
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -111,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
